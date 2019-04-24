6150aca5d20c6f335011e16f7d68e0d188d719d9
Michael Prentice <splaktar@gmail.com>
2019-04-24 04:35:09 -0500

    refactor: adjustments to landing page layout
    add a variables Sass partial
    set up sponsor's template to handle an array of sponsors
    update angular, rxjs, firebase, angulartics2
    don't require firebase-tools installed globally
    
    Relates to #21

--- projects/web/src/app/sponsors/sponsors.component.ts
+++ projects/web/src/app/sponsors/sponsors.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sponsors.component.scss']
 })
 export class SponsorsComponent implements OnInit {
+  // TODO consider eventually moving this to Firebase
+  public sponsors = [
+    {
+      url: 'https://cloud.google.com/',
+      alt: 'Logo de Google Cloud',
+      image: 'google-cloud-platform.svg'
+    },
+    {
+      url: 'https://www.devintent.com/',
+      alt: 'Logo de DevIntent',
+      image: 'DevIntentLogo_40h.png'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
