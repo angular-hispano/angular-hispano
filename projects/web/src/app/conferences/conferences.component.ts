2648339edf0e769792961b98c8188955cf65614e
Michael Prentice <splaktar@gmail.com>
2019-06-05 03:21:36 -0400

    refactor(conferences): align action buttons
    order by date

--- projects/web/src/app/conferences/conferences.component.ts
+++ projects/web/src/app/conferences/conferences.component.ts
@@ -9,20 +9,12 @@ export class ConferencesComponent implements OnInit {
   conferences = [
     {
       name: 'NG Bolivia',
-      logoUrl: 'https://ng-bolivia.org/images/logos/ng-bo.png',
+      logoUrl: 'assets/ng-bo.png',
       dateString: '23 y 24 de agosto, 2019',
       siteUrl: 'https://ng-bolivia.org/',
       ticketsUrl: undefined,
       cfpUrl:
           'https://docs.google.com/forms/d/e/1FAIpQLSeXyM2y97HYshC8aHAeGOdUOC6Qf2y8yUpyZfNZbB00LtpG_w/viewform?usp=sf_link'
-    },
-    {
-      name: 'ngSpain',
-      logoUrl: 'https://ngspain.com/images/logo.svg',
-      dateString: '19 y 20 de octubre, 2019',
-      siteUrl: 'https://ngspain.com/',
-      ticketsUrl: undefined,
-      cfpUrl: 'https://forms.gle/87xT2jwdQgCKtTcZA'
     },
     {
       name: 'NG Honduras',
@@ -31,7 +23,15 @@ export class ConferencesComponent implements OnInit {
       siteUrl: 'https://ng-honduras.org',
       ticketsUrl: undefined,
       cfpUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSej6MAID9G4DwH1YEFlEZuJK-5mGV3Tzq729XDh-OdEAR6qgQ/viewform'
-     }
+    },
+    {
+      name: 'ngSpain',
+      logoUrl: 'https://ngspain.com/images/logo.svg',
+      dateString: '19 y 20 de octubre, 2019',
+      siteUrl: 'https://ngspain.com/',
+      ticketsUrl: undefined,
+      cfpUrl: 'https://forms.gle/87xT2jwdQgCKtTcZA'
+    }
   ];
 
   constructor() {}
