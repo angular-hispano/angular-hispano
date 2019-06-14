45acbe2b077e801ebcba3c28efa804678f1f9ad2
Michael Prentice <splaktar@gmail.com>
2019-06-13 20:56:13 -0400

    build: enable Ivy
    migrate workspace for @angular/core@9.0.0-beta
    Decorates all declared undecorated provider classes with @Injectable
    Angular CLI update
    @angular/material@next
    - adjust sidenav width to match material design spec
    - fix tests broken by change to Angular Material `9.0.0-next.0`

--- projects/web/src/app/landing/landing.component.spec.ts
+++ projects/web/src/app/landing/landing.component.spec.ts
@@ -1,5 +1,5 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {MatIconModule} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
 
 import {LandingComponent} from './landing.component';
 import {SponsorsComponent} from '../sponsors/sponsors.component';
