4a6f21af4431772c140a6690eec9c3ba3ce0d061
devAxel <edisonfernandez.1988@hotmail.com>
2019-07-10 15:16:06 -0500

    feact(cfp): cambio campos ticketsUrl y cfpUrl a null
    Actualiza campos ticketsUrl y cfpUrl a null de CFPs cerrados

--- projects/web/src/app/conferences/conferences.component.ts
+++ projects/web/src/app/conferences/conferences.component.ts
@@ -12,8 +12,8 @@ export class ConferencesComponent implements OnInit {
       logoUrl: 'assets/ng-bo.png',
       dateString: '23 y 24 de agosto, 2019',
       siteUrl: 'https://ng-bolivia.org/',
-      ticketsUrl: undefined,
-      cfpUrl: '',
+      ticketsUrl: null,
+      cfpUrl: null,
       country: 'Bolivia',
       city: 'Cochabamba,'
     },
@@ -22,7 +22,7 @@ export class ConferencesComponent implements OnInit {
       logoUrl: 'https://ng-honduras.org/images/logo.svg',
       dateString: '18 y 19 de octubre, 2019',
       siteUrl: 'https://ng-honduras.org',
-      ticketsUrl: undefined,
+      ticketsUrl: null,
       cfpUrl:
           'https://docs.google.com/forms/d/e/1FAIpQLSej6MAID9G4DwH1YEFlEZuJK-5mGV3Tzq729XDh-OdEAR6qgQ/viewform',
       country: 'Honduras',
@@ -33,8 +33,8 @@ export class ConferencesComponent implements OnInit {
       logoUrl: 'https://ngspain.com/images/logo.svg',
       dateString: '19 y 20 de octubre, 2019',
       siteUrl: 'https://ngspain.com/',
-      ticketsUrl: undefined,
-      cfpUrl: '',
+      ticketsUrl: null,
+      cfpUrl: null,
       country: 'España',
       city: 'Madrid,'
     }
