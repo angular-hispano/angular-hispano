102f4f4a73ebc508c9cee4b1d1bed444b830673e
devAxel <edisonfernandez.1988@hotmail.com>
2019-07-10 12:34:08 -0500

    refactor(cfp): elimina enlaces de CFPs cerrados
    ngSpain y NG Bolivia han cerrado CFPs

--- projects/web/src/app/conferences/conferences.component.ts
+++ projects/web/src/app/conferences/conferences.component.ts
@@ -13,8 +13,7 @@ export class ConferencesComponent implements OnInit {
       dateString: '23 y 24 de agosto, 2019',
       siteUrl: 'https://ng-bolivia.org/',
       ticketsUrl: undefined,
-      cfpUrl:
-          'https://docs.google.com/forms/d/e/1FAIpQLSeXyM2y97HYshC8aHAeGOdUOC6Qf2y8yUpyZfNZbB00LtpG_w/viewform?usp=sf_link',
+      cfpUrl: '',
       country: 'Bolivia',
       city: 'Cochabamba,'
     },
@@ -35,7 +34,7 @@ export class ConferencesComponent implements OnInit {
       dateString: '19 y 20 de octubre, 2019',
       siteUrl: 'https://ngspain.com/',
       ticketsUrl: undefined,
-      cfpUrl: 'https://forms.gle/87xT2jwdQgCKtTcZA',
+      cfpUrl: '',
       country: 'España',
       city: 'Madrid,'
     }
