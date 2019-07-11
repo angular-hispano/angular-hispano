5296940e53eb04043ee2aa19fb19831bb950f71e
Edison Fernández <AXEL1988@users.noreply.github.com>
2019-07-11 12:56:53 -0500

    refactor(cfp): elimina enlaces de CFPs cerrados (#75)
    refactor(cfp): elimina enlaces de CFPs cerrados
    
    ngSpain y NG Bolivia han cerrado CFPs
    
    Closes #74

--- projects/web/src/app/conferences/conferences.component.ts
+++ projects/web/src/app/conferences/conferences.component.ts
@@ -12,9 +12,8 @@ export class ConferencesComponent implements OnInit {
       logoUrl: 'assets/ng-bo.png',
       dateString: '23 y 24 de agosto, 2019',
       siteUrl: 'https://ng-bolivia.org/',
-      ticketsUrl: undefined,
-      cfpUrl:
-          'https://docs.google.com/forms/d/e/1FAIpQLSeXyM2y97HYshC8aHAeGOdUOC6Qf2y8yUpyZfNZbB00LtpG_w/viewform?usp=sf_link',
+      ticketsUrl: null,
+      cfpUrl: null,
       country: 'Bolivia',
       city: 'Cochabamba,'
     },
@@ -23,7 +22,7 @@ export class ConferencesComponent implements OnInit {
       logoUrl: 'https://ng-honduras.org/images/logo.svg',
       dateString: '18 y 19 de octubre, 2019',
       siteUrl: 'https://ng-honduras.org',
-      ticketsUrl: undefined,
+      ticketsUrl: null,
       cfpUrl:
           'https://docs.google.com/forms/d/e/1FAIpQLSej6MAID9G4DwH1YEFlEZuJK-5mGV3Tzq729XDh-OdEAR6qgQ/viewform',
       country: 'Honduras',
@@ -34,8 +33,8 @@ export class ConferencesComponent implements OnInit {
       logoUrl: 'https://ngspain.com/images/logo.svg',
       dateString: '19 y 20 de octubre, 2019',
       siteUrl: 'https://ngspain.com/',
-      ticketsUrl: undefined,
-      cfpUrl: 'https://forms.gle/87xT2jwdQgCKtTcZA',
+      ticketsUrl: null,
+      cfpUrl: null,
       country: 'España',
       city: 'Madrid,'
     }
