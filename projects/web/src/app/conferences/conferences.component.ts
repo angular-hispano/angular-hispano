8d0586de989521dc992f1408d2d97fdd50505d35
Michael Prentice <splaktar@gmail.com>
2019-11-10 22:04:37 -0500

    refactor: quitar ng-spain, actualizar ng-honduras

--- projects/web/src/app/conferences/conferences.component.ts
+++ projects/web/src/app/conferences/conferences.component.ts
@@ -8,23 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class ConferencesComponent implements OnInit {
   conferences = [
     {
-      name: 'ngSpain',
-      logoUrl: 'https://ngspain.com/images/logo.svg',
-      logoType: 'svg',
-      dateString: '19 y 20 de octubre, 2019',
-      siteUrl: 'https://ngspain.com/',
-      ticketsUrl: 'https://ti.to/ngspain/2019',
-      cfpUrl: null,
-      country: 'España',
-      city: 'Madrid'
-    },
-    {
       name: 'NG Honduras',
       logoUrl: 'assets/ng-honduras-logo.svg',
       logoType: 'svg',
-      dateString: 'febrero, 2020',
+      dateString: '28 de febrero, 2020',
       siteUrl: 'https://ng-honduras.org',
-      ticketsUrl: 'https://www.eventbrite.es/e/entradas-ng-honduras-2019-62088406094',
+      ticketsUrl: 'https://www.eventbrite.es/e/entradas-ng-honduras-2020-62088406094',
       cfpUrl: null,
       country: 'Honduras',
       city: 'San Pedro Sula'
