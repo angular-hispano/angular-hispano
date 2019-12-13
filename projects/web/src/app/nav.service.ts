11b6546f0aa5924e1036d786e936272560e1849b
Michael Prentice <splaktar@gmail.com>
2019-12-12 23:10:34 -0500

    build: enable strictTemplates, strictFunctionTypes, and strictPropertyInitialization

--- projects/web/src/app/nav.service.ts
+++ projects/web/src/app/nav.service.ts
@@ -1,26 +1,44 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Injectable } from '@angular/core';
-import { MatDrawer } from '@angular/material/sidenav';
+import { MatDrawer, MatDrawerToggleResult } from '@angular/material/sidenav';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { FocusOrigin } from '@angular/cdk/a11y';
 
 @Injectable({ providedIn: 'root' })
 export class NavService {
-  private _drawer: MatDrawer;
+  private _drawer: MatDrawer | undefined;
+  public get drawer(): MatDrawer | undefined {
+    return this._drawer;
+  }
+
   public isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
-      shareReplay()
+      shareReplay(1)
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
 
-  public get drawer(): MatDrawer {
-    return this._drawer;
+  public setDrawer(value: MatDrawer) {
+    this._drawer = value;
   }
 
-  public set drawer(value: MatDrawer) {
-    this._drawer = value;
+  public closeDrawer(): Promise<MatDrawerToggleResult> | null {
+    if (this.drawer) {
+      return this.drawer.close();
+    }
+    return null;
+  }
+
+  public toggleDrawer(
+    isOpen?: boolean,
+    openedVia?: FocusOrigin
+  ): Promise<MatDrawerToggleResult> | null {
+    if (this.drawer) {
+      return this.drawer.toggle(isOpen, openedVia);
+    }
+    return null;
   }
 }
