fbec8d96474f40bd79eb2740def89f0cec37fed5
Luis Reinoso <lomejordejr@gmail.com>
2020-01-19 16:29:54 -0500

    refactor(sw): Move service worker logic to its own service
    Issue #94

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { Angulartics2GoogleGlobalSiteTag } from 'angulartics2/gst';
 import { environment } from '../environments/environment';
-import { SwUpdate } from '@angular/service-worker';
-import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
-import { timer } from 'rxjs';
+import { SwUpdateService } from './sw-update.service';
 
 @Component({
   selector: 'app-root',
@@ -11,31 +9,13 @@ import { timer } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  private START_CHECK_FOR_UPDATE = 1000; // 1 second
-  private CHECK_FOR_UPDATE_EVERY_FIVE_MINS = 1000 * 60 * 5; // 5 mins
-
   constructor(
     private angulartics: Angulartics2GoogleGlobalSiteTag,
-    private updates: SwUpdate,
-    private snackBar: MatSnackBar
+    private swUpdateService: SwUpdateService
   ) {
     if (environment.production) {
       angulartics.startTracking();
-      this.checkfAppUpdates();
-    }
-  }
-
-  private checkfAppUpdates() {
-    if ('serviceWorker' in navigator) {
-      timer(this.START_CHECK_FOR_UPDATE, this.CHECK_FOR_UPDATE_EVERY_FIVE_MINS).subscribe(() =>
-        this.updates.checkForUpdate()
-      );
-      this.updates.available.subscribe(event => {
-        this.snackBar
-          .open('Hay una versión nueva disponible', 'ACTUALIZAR')
-          .onAction()
-          .subscribe(() => this.updates.activateUpdate().then(() => document.location.reload()));
-      });
+      this.swUpdateService.checkForUpdate();
     }
   }
 }
