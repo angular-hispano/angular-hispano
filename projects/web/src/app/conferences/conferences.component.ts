a59b4fadf59347340e622d7dd9b6a590a7c7ddc6
Michael Prentice <splaktar@gmail.com>
2020-02-09 23:00:29 -0500

    feat: añadir ng conf co y ngSpain 2020

--- projects/web/src/app/conferences/conferences.component.ts
+++ projects/web/src/app/conferences/conferences.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Conference } from './conference';
 
 @Component({
   selector: 'app-conferences',
@@ -6,17 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conferences.component.scss']
 })
 export class ConferencesComponent implements OnInit {
-  conferences = [
+  conferences: Conference[] = [
     {
       name: 'NG Honduras',
       logoUrl: 'assets/ng-honduras-logo.svg',
       logoType: 'svg',
-      dateString: '28 de febrero, 2020',
+      dateString: '28-29 de febrero, 2020',
       siteUrl: 'https://ng-honduras.org',
       ticketsUrl: 'https://www.eventbrite.es/e/entradas-ng-honduras-2020-62088406094',
       cfpUrl: null,
       country: 'Honduras',
       city: 'San Pedro Sula'
+    },
+    {
+      name: 'Ng Conf Colombia',
+      logoUrl: 'https://ngconf.co/assets/logo-01.png',
+      logoType: 'png',
+      dateString: '14-15 de agosto, 2020',
+      siteUrl: 'https://ngconf.co',
+      ticketsUrl: null,
+      cfpUrl: null,
+      country: 'Colombia',
+      city: 'Medellín'
+    },
+    {
+      name: 'ngSpain',
+      logoUrl: 'https://ngspain.com/images/logo.svg',
+      logoType: 'svg',
+      dateString: 'octubre, 2020',
+      siteUrl: 'https://ngspain.com',
+      ticketsUrl: null,
+      cfpUrl: null,
+      country: 'España',
+      city: 'Málaga'
     }
   ];
 
