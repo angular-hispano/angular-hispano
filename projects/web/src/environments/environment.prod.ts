b9fdcfc4e70c343aa94df0f213c5396b2b456bd9
Michael Prentice <splaktar@gmail.com>
2020-06-14 05:05:40 -0400

    chore: actualizar apiKey y añadir measurementId para Firebase

--- projects/web/src/environments/environment.prod.ts
+++ projects/web/src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCCOKoq4IZydpU5uchHCFUDKV7ZMcvKszc',
+    apiKey: 'AIzaSyDQ4JdM82Knpe5sBuy7XDvfpje20U9KRA4',
     authDomain: 'angular-latino.firebaseapp.com',
     databaseURL: 'https://angular-latino.firebaseio.com',
     projectId: 'angular-latino',
     storageBucket: 'angular-latino.appspot.com',
     messagingSenderId: '1050547922973',
-    appId: '1:1050547922973:web:52c1921415662799'
+    appId: '1:1050547922973:web:52c1921415662799',
+    measurementId: 'G-N4MTNR2XNC'
   }
 };
